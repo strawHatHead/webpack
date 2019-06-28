98fe00ec24a1e6a2f34bb3d68b474c47193e03d3
sbb <511038368@qq.com>
2019-06-28 14:31:02 +0800

    webpack初级-热更新处理

--- src/search.js
+++ src/search.js
@@ -1,1 +1,21 @@
-document.write('search page');
+'user strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './search.less';
+import imgs from './images/3.jpg';
+
+class Search extends React.Component{
+    render () {
+        return (<div className="search">
+            search pages
+            工业也米娜
+            <img src={imgs} />
+        </div>)
+    }
+}
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const webpack = require('webpack');
 module.exports = {
     entry: {
         index: './src/index.js',
@@ -10,13 +11,64 @@ module.exports = {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js'
     },
-    mode: 'production',
+    mode: 'development',
     module: {
         rules: [
             {
-                test: '/\.txt$/',
+                test: /\.txt$/,
                 use: 'raw-loader'
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
+            // {
+            //     test: /\.(png|gif|svg|jpg)$/,
+            //     use: 'file-loader'
+            // },
+            {
+                test: /\.(png|gif|svg|jpg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 102400
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.(otf|woff|woff2|eot|ttf)$/,
+                use: 'file-loader'
             }
         ]
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+        aggregateTimeout: 300,
+        poll: 1000
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
     }
 };
